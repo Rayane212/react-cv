6f766bb0522852bbe56cb6c71d5e0e647abba20c
Rayane <86132260+Rayane212@users.noreply.github.com>
2023-05-05 12:34:24 +0000

    maj otherskill

--- src/components/skills/OtherSkills.js
+++ src/components/skills/OtherSkills.js
@@ -6,16 +6,14 @@ const OtherSkills = () => {
             <h3>Autres compétences</h3>
             <div className="list">
                 <ul>
-                    <li><i className="fas fa-check-square"></i>Anglais courant</li>
+                    <li><i className="fas fa-check-square"></i>Anglais intermédiaire</li>
                     <li><i className="fas fa-check-square"></i>GitHub</li>
-                    <li><i className="fas fa-check-square"></i>SEO</li>
                     <li><i className="fas fa-check-square"></i>Méthodes agiles</li>
                 </ul>
                 <ul>
                 <li><i className="fas fa-check-square"></i>Figma</li>
                 <li><i className="fas fa-check-square"></i>UI/UX Design</li>
-                <li><i className="fas fa-check-square"></i>Photoshop</li>
-                <li><i className="fas fa-check-square"></i>Montage Vidéo</li>
+                <li><i className="fas fa-check-square"></i>DevOps</li>
                 </ul>
             </div>
         </div>
